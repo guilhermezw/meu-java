e8561e9135ebe2ed027c001863d014eccf62e959
Guilherme <guifefo@gmail.com>
2026-01-05 21:23:43 -0300

    .

---
+++  use-a-cabeca-java/src/main/java/modulo2/Episode.java
@@ -0,0 +1,18 @@
+package modulo2;
+
+public class Episode {
+    int seriesNumber;
+    int episodeNumber;
+
+    void play(){
+        System.out.println("Playing episode " + episodeNumber);
+    }
+
+    void skipIntro() {
+        System.out.println("Skipping intro... ");
+    }
+
+    void skipToNext() {
+        System.out.println("Loading next episode...");
+    }
+}
